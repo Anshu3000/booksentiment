9bbe6e267551a7595267b40184e7f1cd3850f165
Anshulight <mrray7838035317@gmail.com>
2021-02-14 12:23:29 +0530

    firebse verification signin and signup working successfull.

--- app/src/main/java/com/example/aiproject/Signin12.java
+++ app/src/main/java/com/example/aiproject/Signin12.java
@@ -1,8 +1,5 @@
 package com.example.aiproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,27 +8,30 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+
+public class Signin12 extends AppCompatActivity {
+
+    private TextView entertextt1;
+    private TextInputEditText email1,password1;
     private MaterialButton b1;
-    private TextView   signintext1;
-    private TextInputEditText email1,password1,repassword1;
     private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signin12);
 
-        intial1();
-
+        intit2();
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,10 +40,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        signintext1.setOnClickListener(new View.OnClickListener() {
+
+        entertextt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), Signin12.class);
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 overridePendingTransition(R.anim.push_right_in,R.anim.push_left_in);
                 startActivity(i);
                 finish();
@@ -52,12 +53,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-
     private void create1() {
+
         String emai= String.valueOf(email1.getText());
         String pass1= String.valueOf(password1.getText());
-        String pass2= String.valueOf(repassword1.getText());
+
 
         if(emai.isEmpty()){
             email1.setError("PLease enter email");
@@ -68,81 +68,54 @@ public class MainActivity extends AppCompatActivity {
             password1.setError("please enter password");
             password1.requestFocus();
             return ;
-        }else if(pass2.isEmpty()){
-            repassword1.setError("please retype a password");
-            repassword1.requestFocus();
-            return ;
         }
         email1.setError(null);
         password1.setError(null);
-        repassword1.setError(null);
+
 
         if(!(android.util.Patterns.EMAIL_ADDRESS.matcher(emai).matches())){
             email1.setError("Please enter right email address ");
-            email1.requestFocus();
-            return;
-        }else if(!pass1.equals(pass2)){
-           Toast t1= Toast.makeText(this, "password and retype passord should be same..", Toast.LENGTH_SHORT);
-            t1.setGravity(Gravity.CENTER, 0, 0);
-            t1.show();
-            repassword1.setError("password did not match ");
-            repassword1.requestFocus();
             return;
         }
 
-        repassword1.setError(null);
         email1.setError(null);
 
+        //user signin...
 
-        //here we enter a data
-
-        mAuth.createUserWithEmailAndPassword(emai, pass1)
+        mAuth.signInWithEmailAndPassword(emai, pass1)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Log.d("succes", "createUserWithEmail:success");
-//                            FirebaseUser user = mAuth.getCurrentUser();
-                            Intent i = new Intent(getApplicationContext(), Signin12.class);
-                            overridePendingTransition(R.anim.push_right_in,R.anim.push_left_in);
-                            startActivity(i);
-                            finish();
-//
+                            Log.d("succ1", "signInWithEmail:success");
+                          //  FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), "Authentication succesfull..",
+                                    Toast.LENGTH_SHORT).show();
 
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.e("erro105", "createUserWithEmail:failure", task.getException());
-
+                            Log.w("error1", "signInWithEmail:failure", task.getException());
                             Toast.makeText(getApplicationContext(), "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
 
+                            // ...
                         }
 
+                        // ...
                     }
                 });
 
 
+
     }
 
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            //TODO create a user already register then what to do
-
-        }
-    }
-
-    private void intial1() {
-        mAuth=FirebaseAuth.getInstance();
-
-        b1=findViewById(R.id.signup1);
-        signintext1=findViewById(R.id.text12sig);
-        email1=findViewById(R.id.emailaddr1);
-        password1=findViewById(R.id.pass1);
-        repassword1=findViewById(R.id.pass2);
+    private void intit2() {
+        mAuth = FirebaseAuth.getInstance();
+        entertextt1=findViewById(R.id.entertext12);
+        email1=findViewById(R.id.enteremailaddr1);
+        password1=findViewById(R.id.enterpassword1);
+        b1=findViewById(R.id.signin1);
     }
 }
